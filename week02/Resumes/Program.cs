c82307d05555bcfc75c91288e0fed9d0ac00241c
Hans Barredo <hansit.barredo@gmail.com>
2025-01-13 12:10:30 +0800

    week 2 learninga ctivity code

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume myResume = new Resume();
+        myResume._name = "Hans Barredo";
+
+        Job job1 = new Job()
+        {
+        _jobTitle ="Sotware Engineer",
+        _company = "Apple",
+        _startYear = 2019,
+        _endYear = 2020,
+        };
+
+        Job job2 = new Job
+        {
+            _jobTitle = "Software Engineer",
+            _company = "Microsoft",
+            _startYear = 2020,
+            _endYear = 2021
+        };
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
