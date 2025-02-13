431f47940cb368908b97745d37b4fdd025b1cae8
Hans Barredo <hansit.barredo@gmail.com>
2025-02-13 09:25:47 +0800

    week six learninga ctivities

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s1 = new Square();
+        s1.SetColor("blue");
+        s1.SetSide(45);
+        Rectangle r1 = new Rectangle();
+        r1.SetColor("red");
+        r1.SetLength(15);
+        r1.SethWidth(30);
+        Circle c1 = new Circle();
+        c1.SetColor("green");
+        c1.SetRadius(15);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(s1);
+        shapes.Add(r1);
+        shapes.Add(c1);
+
+         foreach(Shape shape in shapes)
+         {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine($"Area = {area} \nColor = {color}");
+         }
     }
+    
 }
---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,26 @@
+class Rectangle : Shape
+{
+    private double _length;
+    private double _width;
+
+    public double GetLength(){
+        return _length;
+    }
+
+    public void SetLength(double length){
+        _length = length;
+    }
+
+    public double GetWidth(){
+        return _width;
+    }
+
+    public void SethWidth(double width){
+        _width = width;
+    }
+    
+    public override double GetArea()
+    {
+        return _length * _width;
+    }
+}
