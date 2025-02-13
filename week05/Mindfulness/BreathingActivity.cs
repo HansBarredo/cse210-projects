3405a6ebe1157f5fb798e4f7ffa94c22ffc20499
Hans Barredo <hansit.barredo@gmail.com>
2025-02-13 09:25:35 +0800

    week seek learning activity

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,7 +1,7 @@
 using System;
 class BreathingActivity : Activity
 {
-    public BreathingActivity() : base()
+    public BreathingActivity()
     {
         _name = "Breathing Activity\n";
         _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.\n";
